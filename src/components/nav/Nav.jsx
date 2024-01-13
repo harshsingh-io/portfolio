4cc1a2b2606cd51185af443756cc69ceceaea1de
Harsh Singh <Harshsingh704888@gmail.com>
2024-01-14 00:47:01 +0530

    Updated Portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,11 +11,14 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav==='#' ? 'active' : ''}><AiOutlineHome/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href="#experience"onClick={() => setActiveNav('#experience')} className={activeNav==='#experience' ? 'active' : ''}><MdOutlineBook/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav==='#services' ? 'active' : ''}><RiServiceLine/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+      
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav==='#' ? 'active' : ''}><AiOutlineHome/><span>Home</span></a>
+
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><AiOutlineUser/><span>About Me</span></a>
+      <a href="#experience"onClick={() => setActiveNav('#experience')} className={activeNav==='#experience' ? 'active' : ''}><MdOutlineBook/><span>Skills</span></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav==='#portfolio' ? 'active' : ''}><RiServiceLine/><span>Projects</span></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><BiMessageSquareDetail/><span>Contact</span></a>
+      
     </nav>
   )
 }
--- src/components/portfolio/Porfolio.jsx
+++ src/components/portfolio/Porfolio.jsx
@@ -9,6 +9,7 @@ import DOODLEIT from"../../assets/doodleit.webp"
 import HAPPYPLACE from "../../assets/P2.webp"
 import WEATHER from "../../assets/P4.webp"
 import TRELLO from "../../assets/P6.webp"
+import ECOSYNC from "../../assets/EcoSync.webp"
 
 const data = [
 
@@ -16,52 +17,59 @@ const data = [
   {
     id:1,
     image: TRELLO,
-    title: 'KanbanBoard: Project Management Tool',
+    title: 'Trello Clone: KanbanBoard',
     github: 'https://github.com/harshsingh-io/KanbanBoard',
-    demo: 'https://github.com/harshsingh-io/KanbanBoard#preview'
+    demo: 'https://github.com/harshsingh-io/KanbanBoard/blob/main/README.md#demo'
   },
   {
     id:2,
-    image: WEATHER,
-    title: 'WeatherCast: Weather Forcasting App',
-    github: 'https://github.com/harshsingh-io/WeatherCast',
-    demo: 'https://github.com/harshsingh-io/WeatherCast#preview'
+    image: ECOSYNC,
+    title: 'EcoSync: Electricity Tracking App',
+    github: 'https://github.com/harshsingh-io/EcoSync',
+    demo: 'https://github.com/harshsingh-io/EcoSync/blob/main/README.md#demo'
   },
   {
     id:3,
-    image: HAPPYPLACE,
-    title: 'MemoryWhisper: HappyPlace App',
-    github: 'https://github.com/harshsingh-io/MemoryWhisper',
-    demo: 'https://github.com/harshsingh-io/MemoryWishper#preview'
+    image: WEATHER,
+    title: 'WeatherCast: Weather Forcasting App',
+    github: 'https://github.com/harshsingh-io/WeatherCast',
+    demo: 'https://github.com/harshsingh-io/WeatherCast/blob/main/README.md#demo'
   },
   {
     id:4,
-    image: WORKOUT,
-    title: 'BodySync: Workout App',
-    github: 'https://github.com/harshsingh-io/BodySync',
-    demo: 'https://github.com/harshsingh-io/BodySync#preview'
+    image: HAPPYPLACE,
+    title: 'MemoryWhisper: HappyPlace App',
+    github: 'https://github.com/harshsingh-io/MemoryWhisper',
+    demo: 'https://github.com/harshsingh-io/MemoryWhisper/tree/main#demo'
   },
   {
     id:5,
-    image: DOODLEIT,
-    title: 'DoodleIt: Drawing App',
-    github: 'https://github.com/harshsingh-io/DoodleIt',
-    demo: 'https://github.com/harshsingh-io/DoodleIt#preview'
+    image: WORKOUT,
+    title: 'BodySync: Workout App',
+    github: 'https://github.com/harshsingh-io/BodySync',
+    demo: 'https://github.com/harshsingh-io/BodySync/blob/main/README.md#demo'
   },
   {
     id:6,
-    image: QUIZIE,
-    title: 'Quizie: Quiz App',
-    github: 'https://github.com/harshsingh-io/quizie',
-    demo: '#https://github.com/harshsingh-io/quizie#preview'
+    image: DOODLEIT,
+    title: 'DoodleIt: Drawing App',
+    github: 'https://github.com/harshsingh-io/DoodleIt',
+    demo: 'https://github.com/harshsingh-io/DoodleIt/blob/main/README.md#demo'
   },
   {
     id:7,
+    image: QUIZIE,
+    title: 'Quizie: Quiz App',
+    github: 'https://github.com/harshsingh-io/quizie',
+    demo: '#https://github.com/harshsingh-io/quizie/blob/main/README.md#demo'
+  },
+  {
+    id:8,
     image: CALCI,
     title: 'Calculator',
     github: 'https://github.com/harshsingh-io/calculator',
-    demo: 'https://github.com/harshsingh-io/raw/raw/main/calcidemo.mp4'
-  },
+    demo: 'https://github.com/harshsingh-io/calculator/blob/main/README.md#demo'
+  }
   
 ]
 
